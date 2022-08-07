5b367ce6233ee3f6377cf9c5a6f40f10da8e4234
Jeff Wiedemann <fej15@gmail.com>
2022-08-07 22:02:34 +0100

    reading input file one line at a time

--- sendusb.py
+++ sendusb.py
@@ -12,10 +12,11 @@ print(ser.name)         # check which port was really used
 #   ser.write(str.encode('bye\n'))
 #   ser.write(str.encode('53:02.0,41.86573608,-87.60680976,2022-04-30T18:53:02.000Z,187.835,0.043,-0.014,73.3248,2.987,4.953,0.4,nan,15.1\n'))
 with open('../GPSLog/GPSLog08.csv') as f:
-  lines = f.read()
-  ser.write(str.encode(lines))
-  sleep(1) 
-ser.write(b'done')     # write a string
+  for lines in f:
+     #lines = f.read()
+     ser.write(str.encode(lines))
+     sleep(1) 
+  ser.write(b'done')     # write a string
 
 
 ser.close()             # close port
