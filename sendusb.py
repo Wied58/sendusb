730c1c2fd8e10b09e811bc521729d9ec6bf078f3
Jeff Wiedemann <fej15@gmail.com>
2022-08-07 02:56:42 +0100

    read file and write lines to ttyUSB0

--- sendusb.py
+++ sendusb.py
@@ -7,13 +7,14 @@ from time import sleep
 #ser = serial.Serial('/dev/ttyUSB0')  # open serial port
 ser=serial.Serial('/dev/ttyUSB0',9600)
 
-for x in range(10):
-   print(ser.name)         # check which port was really used
+print(ser.name)         # check which port was really used
 #   ser.write(b'hello\n')     # write a string
 #   ser.write(str.encode('bye\n'))
-   ser.write(str.encode('53:02.0,41.86573608,-87.60680976,2022-04-30T18:53:02.000Z,187.835,0.043,-0.014,73.3248,2.987,4.953,0.4,nan,15.1\n'))
-#   ser.write(serialcmd.encode())
-   sleep(1) 
+#   ser.write(str.encode('53:02.0,41.86573608,-87.60680976,2022-04-30T18:53:02.000Z,187.835,0.043,-0.014,73.3248,2.987,4.953,0.4,nan,15.1\n'))
+with open('../GPSLog/GPSLog08.csv') as f:
+  lines = f.read()
+  ser.write(str.encode(lines))
+  sleep(1) 
 ser.write(b'done')     # write a string
 
 
